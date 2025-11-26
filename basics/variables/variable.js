d552f0a3bec286bffa0232fa8dd2b5738e3acfde
GONNE <gonne411@gmail.com>
2025-11-26 03:48:23 +0200

    added some note on the code file variable.js

--- basics/variables/variable.js
+++ basics/variables/variable.js
@@ -28,12 +28,39 @@ const favoriteSubject = "Computer Science";
 const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
 console.log(favoriteSubjectSentence);
 
-//concat() method is another way to combine strings
-// const excitedGreeting = "Hello! ".concat("I am very excited to learn ").concat(favoriteSubject).concat("!");
-// console.log(excitedGreeting);
+/*concat() method is another way to combine strings
+const excitedGreeting = "Hello! ".concat("I am very excited to learn ").concat(favoriteSubject).concat("!");
+console.log(excitedGreeting);
 
-// += operator to append strings
-// let excitedGreeting = "Hello! ";
-// excitedGreeting += "I am very excited to learn ";
-// the output will be hello! I am very excited to learn Computer Science!
++= operator to append strings or add to existing strings
+let excitedGreeting = "Hello! ";
+excitedGreeting += "I am very excited to learn ";
+the output will be hello! I am very excited to learn Computer Science!
+
+to check the data type of a variable, you can use the typeof operator
+console.log(typeof bot); // Output: string
+console.log(typeof favoriteSubject); // Output: string
+
+JavaScript is case-sensitive, so be careful with variable names like bot and Bot,which would be considered different variables.
+javascript has the following primitive data types: string, number, boolean, null, undefined, symbol, and bigint.
+
+null represents the intentional absence of any object value,
+while undefined indicates that a variable has been declared but not assigned a value.
+
+symbol is a unique and immutable primitive value often used as the key of an object property to avoid name collisions. like this:
+const uniqueId = Symbol('id');
+const obj = {
+  [uniqueId]: 123
+};
+console.log(obj[uniqueId]); // Output: 123
+
+bigint is used to represent integers larger than the maximum safe integer limit for the number type.
+You can create a bigint by appending 'n' to the end of an integer literal, like this:
+const bigNumber = 9007199254741991n;
+console.log(bigNumber); // Output: 9007199254741991n;
+
+camelCase is a common convention for naming variables in JavaScript, where the first word is lowercase and
+each subsequent word starts with an uppercase letter, like favoriteSubject.
+when you check the type of null using typeof, it returns "object", which is a known quirk in JavaScript.
+ */
 
